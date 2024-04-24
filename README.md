361c9d1c407bdbac3c25a9811d70c8dce01539e8
cql <1968340219@qq.com>
2024-04-24 20:38:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+<!--
+ * @Author: 陈巧龙
+ * @Date: 2024-04-24 20:29:44
+ * @LastEditors: 
+ * @LastEditTime: 2024-04-24 20:35:09
+ * @FilePath: \react-app\README.md
+ * @Description: 
+-->
+# React 学习
+
+## 项目简介
+
+1、学习React。
+
+2、部分代码未进行展示，目前项目正在优化中，后续再进行更新。
+
+3、欢迎大家一起沟通交流。
+
+## 项目启动
+
+### 依赖安装
+
+> npm install
+
+### 启动项目
+
+> npm start
---
+++ src/App.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: 陈巧龙
+ * @Date: 2024-04-24 20:29:44
+ * @LastEditors: 
+ * @LastEditTime: 2024-04-24 20:36:57
+ * @FilePath: \react-app\src\App.js
+ * @Description: 
+ */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        Learn React
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
